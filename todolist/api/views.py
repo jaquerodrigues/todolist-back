c6b1bd70c3d1cc6d917a9873ebb9864484fca572
Jaqueline Rodrigues <grodrigues.jaqueline@gmail.com>
2019-09-15 11:35:20 -0300

    trying to add

--- todolist/api/views.py
+++ todolist/api/views.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from todolist.api.serializers import TaskSerializer
 from .models import Task
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class TaskViewSet(viewsets.ModelViewSet):
@@ -13,5 +16,18 @@ class TaskViewSet(viewsets.ModelViewSet):
     """
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+    @action(detail=True, methods=['post'])
+    def adicionar_tarefa(self, request, pk=None):
+        task = self.get_object()
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            task.adicionar_tarefa(serializer.data['tarefa'])
+            task.save()
+            return Response({'status': 'tarefa add'})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
 
 
